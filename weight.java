a9841ae0efe050cc8ab2fa8320f59d3897e0072c
박지영/j116_201611074 <jyjy0701@gmail.com>
2016-09-26 13:08:05 +0900

    Add files via upload

---
+++ weight.java
@@ -0,0 +1,12 @@
+me.setWeight((float)60.0);
+System.out.println(me.getWeight());
+class Person{
+private float weight;
+public void setWeight(float w){
+this.weight=w;
+}
+private float getWeight(){
+return weight;
+}
+}
+Person me=new Person();
