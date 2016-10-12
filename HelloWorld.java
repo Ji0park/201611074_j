d9d8584a70886fb2e2889c686fca5177e8236dd7
박지영/j116_201611074 <jyjy0701@gmail.com>
2016-10-13 02:27:35 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sd.Hello;
+public class HelloWorld {
+  public void Hello() {
+    System.out.println("Hello World!");
+  }
+  public void Hello(String name) {
+    System.out.printf("Hello %s!", name);
+  }
+  public static void main(String[] args) {
+    System.out.println("Hello, World--");
+  }
+}
